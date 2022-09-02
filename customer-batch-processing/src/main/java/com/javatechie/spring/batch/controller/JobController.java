037696ce22ead022e61bc9e2db0def8a9c284f27
JanarthananeNoah <er.janarthanan04@gmail.com>
2022-09-02 12:12:54 +0530

    Schedule process

--- customer-batch-processing/src/main/java/com/javatechie/spring/batch/controller/JobController.java
+++ customer-batch-processing/src/main/java/com/javatechie/spring/batch/controller/JobController.java
@@ -1,35 +1,110 @@
 package com.javatechie.spring.batch.controller;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
-import org.springframework.batch.core.JobParametersBuilder;
-import org.springframework.batch.core.JobParametersInvalidException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.*;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
 import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
 import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.support.ScheduledMethodRunnable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+import java.util.IdentityHashMap;
+import java.util.Map;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
 @RestController
 @RequestMapping("/job")
 public class JobController {
+
+    private Logger logger = LoggerFactory.getLogger(JobController.class);
+
+    private AtomicBoolean enable = new AtomicBoolean(true);
+
+    private AtomicInteger batchRunCounter = new AtomicInteger(0);
+
+    private final Map<Object, ScheduledFuture<?>> scheduledTask = new IdentityHashMap<>();
 
     @Autowired
     private JobLauncher jobLauncher;
     @Autowired
     private Job job;
 
+    @Scheduled(fixedRate = 10000)
     @PostMapping("/add-details")
-    public void importCsvToDBJob() {
-        JobParameters jobParameters = new JobParametersBuilder()
-                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
-        try {
-            jobLauncher.run(job, jobParameters);
-        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
-            e.printStackTrace();
+    public Job launchJob() throws JobInstanceAlreadyCompleteException, JobExecutionAlreadyRunningException, JobParametersInvalidException, JobRestartException {
+
+        Date date = new Date();
+        logger.debug("Scheduler starts at " + date);
+        if (enable.get()) {
+            JobExecution jobExecution = jobLauncher.run(job, new JobParametersBuilder().addDate("launchDate", date).toJobParameters());
+            batchRunCounter.incrementAndGet();
+
+            logger.debug("Batch job ends with status as " + jobExecution.getStatus());
+        }
+        logger.debug("Scheduler ends");
+        return null;
+    }
+
+    public void stop() {
+        enable.set(false);
+    }
+
+    public void start() {
+        enable.set(true);
+    }
+
+    @Bean
+    public TaskScheduler poolScheduler() {
+        return new CustomTaskScheduler();
+    }
+
+    private class CustomTaskScheduler extends ThreadPoolTaskScheduler {
+        private static final long serialVersionUID = -7142624085505040603L;
+
+        @Override
+        public ScheduledFuture<?> scheduleAtFixedRate(Runnable task, long period) {
+            ScheduledFuture<?> future = super.scheduleAtFixedRate(task, period);
+
+            ScheduledMethodRunnable runnable = (ScheduledMethodRunnable) task;
+            scheduledTask.put(runnable.getTarget(), future);
+
+            return future;
         }
     }
+
+    public void cancelFutureSchedulerTasks() {
+        scheduledTask.forEach((k, v) -> {
+            if (k instanceof JobController) {
+                v.cancel(false);
+            }
+        });
+    }
+
+    public AtomicInteger getBatchRunCounter() {
+        return batchRunCounter;
+    }
+
+
+//        JobParameters jobParameters = new JobParametersBuilder()
+//                .addLong("startAt", System.currentTimeMillis()).toJobParameters();
+//        try {
+//            jobLauncher.run(job, jobParameters);
+//        } catch (JobExecutionAlreadyRunningException | JobRestartException | JobInstanceAlreadyCompleteException | JobParametersInvalidException e) {
+//            e.printStackTrace();
+//        }
+//        return null;
+
 }
